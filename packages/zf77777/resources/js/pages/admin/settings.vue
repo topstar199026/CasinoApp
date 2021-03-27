94c56056a5a1e890343676ed33e6f4b7949c2d74
topstar199026 <topstar199026@gmail.com>
2021-03-28 01:03:32 +0900

    ...

--- packages/zf77777/resources/js/pages/admin/settings.vue
+++ packages/zf77777/resources/js/pages/admin/settings.vue
@@ -15,6 +15,36 @@
               persistent-hint
               @keydown="clearFormErrors($event,'PAYMENTS_ZF777777_USER')"
             />
+            <v-text-field
+              v-model="form.PAYMENTS_ZF77777_NOTIFY_URL"
+              :label="$t('API Notify url')"
+              :error="form.errors.has('PAYMENTS_ZF77777_NOTIFY_URL')"
+              :error-messages="form.errors.get('PAYMENTS_ZF77777_NOTIFY_URL')"
+              outlined
+              :hint="$t('Please note, it is different from your regular zf77777 notify url')"
+              persistent-hint
+              @keydown="clearFormErrors($event,'PAYMENTS_ZF77777_NOTIFY_URL')"
+            />
+            <v-text-field
+              v-model="form.PAYMENTS_ZF77777_RETURN_URL"
+              :label="$t('API return url')"
+              :error="form.errors.has('PAYMENTS_ZF77777_RETURN_URL')"
+              :error-messages="form.errors.get('PAYMENTS_ZF77777_RETURN_URL')"
+              outlined
+              :hint="$t('Please note, it is different from your regular zf77777 return url')"
+              persistent-hint
+              @keydown="clearFormErrors($event,'PAYMENTS_ZF77777_RETURN_URL')"
+            />
+            <v-text-field
+              v-model="form.PAYMENTS_ZF77777_SIGN"
+              :label="$t('API sign')"
+              :error="form.errors.has('PAYMENTS_ZF77777_SIGN')"
+              :error-messages="form.errors.get('PAYMENTS_ZF77777_SIGN')"
+              outlined
+              :hint="$t('Please note, it is different from your regular zf77777 account sign')"
+              persistent-hint
+              @keydown="clearFormErrors($event,'PAYMENTS_ZF77777_SIGN')"
+            />
           </v-expansion-panel-content>
         </v-expansion-panel>
       </v-expansion-panels>
